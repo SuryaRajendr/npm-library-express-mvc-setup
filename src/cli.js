c2409025174eb7e897e58c8ea135419f614b1b14
SuryaRajendr <surya.crajendran@gmail.com>
2024-10-29 12:51:23 +0530

    Swagger added

--- src/cli.js
+++ src/cli.js
@@ -4,17 +4,23 @@ import inquirer from 'inquirer';
 import generateMvcStructure from './generator.js'; // Include .js extension
 
 async function init() {
-  const { language } = await inquirer.prompt([
+  const { language, addSwagger } = await inquirer.prompt([
     {
       type: 'list',
       name: 'language',
       message: 'Which language are you using?',
       choices: ['JavaScript', 'TypeScript'],
     },
+    {
+      type: 'confirm',
+      name: 'addSwagger',
+      message: 'Do you want to add Swagger?',
+      default: false,
+    },
   ]);
 
   const basePath = process.argv[2] || process.cwd();
-  generateMvcStructure(basePath, language);
+  generateMvcStructure(basePath, language, addSwagger);
 }
 
 init();
--- src/generator.js
+++ src/generator.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-function generateMvcStructure(basePath, language) {
+function generateMvcStructure(basePath, language, addSwagger) {
   const ext = language === 'TypeScript' ? 'ts' : 'js';
   const folders = ['controllers', 'models', 'routes', 'middlewares', 'views', 'config'];
 
@@ -19,9 +19,51 @@ function generateMvcStructure(basePath, language) {
   fs.writeFileSync(path.join(basePath, `middlewares/logger.${ext}`), "// Sample middleware");
   fs.writeFileSync(path.join(basePath, `views/index.${ext}`), "// Sample view");
 
+  if (addSwagger) {
+    addSwaggerSetup(basePath, ext);
+    console.log('Swagger setup added.');
+  }
+
   console.log(`MVC structure generated successfully with ${language}!`);
 }
 
-// module.exports = generateMvcStructure;
+function addSwaggerSetup(basePath, ext) {
+  const swaggerConfig = `
+    const swaggerJsDoc = require('swagger-jsdoc');
+    const swaggerUi = require('swagger-ui-express');
+
+    const swaggerOptions = {
+      swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'API Documentation',
+          version: '1.0.0',
+          description: 'API Information',
+        },
+        servers: [{ url: 'http://localhost:3000' }],
+      },
+      apis: ['./routes/*.${ext}'],
+    };
+
+    const swaggerDocs = swaggerJsDoc(swaggerOptions);
+    module.exports = { swaggerUi, swaggerDocs };
+  `;
+
+  const swaggerSetup = `
+    const express = require('express');
+    const { swaggerUi, swaggerDocs } = require('../config/swaggerConfig');
+
+    const app = express();
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  `;
+
+  // Create Swagger config file
+  const configPath = path.join(basePath, `config/swaggerConfig.${ext}`);
+  fs.writeFileSync(configPath, swaggerConfig.trim());
+
+  // Update main app setup with Swagger
+  const appSetupPath = path.join(basePath, `app.${ext}`);
+  fs.writeFileSync(appSetupPath, swaggerSetup.trim());
+}
 
 export default generateMvcStructure;
